98d8a39acff477d5de548d74e0375ca6aecb02e5
ato lash <hal-shu-sato@users.noreply.github.com>
2023-10-30 18:55:48 +0900

    build(next): change the config for SSG

--- next.config.js
+++ next.config.js
@@ -1,4 +1,23 @@
+const prefixPath = process.env.NODE_ENV === "production" ? "/parimutuel-betting-web" : "";
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  output: "export",
+
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  // trailingSlash: true,
+
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  // skipTrailingSlashRedirect: true,
+
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: 'dist',
+
+  assetPrefix: prefixPath,
+  basePath: prefixPath,
+  publicRuntimeConfig: {
+    basePath: prefixPath,
+  },
+};
 
 module.exports = nextConfig
